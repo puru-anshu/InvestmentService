cf3e63f930eade54cb7701cd2a31d4c34884f8d7
anshuman <anshuman.purushottam@gdn-commerce.com>
2024-11-22 11:49:25 +0530

    Initial Commit

---
+++ src/test/java/com/arutech/mftracker/InvestmentService/InvestmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arutech.mftracker.InvestmentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
